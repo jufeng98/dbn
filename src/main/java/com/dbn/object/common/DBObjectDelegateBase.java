20621ec7114c01841707931e350604e19684c55d
Dan Cioca <dancioca@bluewin.ch>
2023-11-14 21:26:00 +0100

    Compatibility issues

--- src/main/java/com/dbn/object/common/DBObjectDelegateBase.java
+++ src/main/java/com/dbn/object/common/DBObjectDelegateBase.java
@@ -1,5 +1,6 @@
 package com.dbn.object.common;
 
+import com.dbn.common.compatibility.Compatibility;
 import com.dbn.object.lookup.DBObjectRef;
 import com.intellij.pom.Navigatable;
 import lombok.experimental.Delegate;
@@ -17,7 +18,20 @@ class DBObjectDelegateBase implements DBObject {
     }
 
     @Override
+    @Compatibility
     public boolean canNavigateToSource() {
         return delegate().canNavigateToSource();
+    }
+
+    @Override
+    @Compatibility
+    public boolean canNavigate() {
+        return delegate().canNavigate();
+    }
+
+    @Override
+    @Compatibility
+    public void navigate(boolean requestFocus) {
+        delegate().navigate(requestFocus);
     }
 }
