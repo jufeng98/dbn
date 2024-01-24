d89128cecde279615fab12cc93da8ce2bd26e053
dancioca <dancioca@bluewin.ch>
2024-01-24 08:14:04 +0100

    Component speed search - caching / model listeners / deprecated code cleanup

--- src/main/java/com/dbn/data/grid/ui/table/basic/BasicTableSpeedSearch.java
+++ src/main/java/com/dbn/data/grid/ui/table/basic/BasicTableSpeedSearch.java
@@ -1,14 +1,17 @@
 package com.dbn.data.grid.ui.table.basic;
 
+import com.dbn.common.compatibility.Compatibility;
 import com.dbn.data.model.ColumnInfo;
 import com.dbn.data.model.DataModelHeader;
 import com.dbn.data.model.basic.BasicDataModel;
 import com.intellij.ui.SpeedSearchBase;
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
 public class BasicTableSpeedSearch extends SpeedSearchBase<BasicTable<? extends BasicDataModel>> {
 
-    private ColumnInfo[] columnInfos;
+    @Getter(lazy = true)
+    private final ColumnInfo[] columnInfos = createColumnInfos();
     private int columnIndex = 0;
 
     public BasicTableSpeedSearch(BasicTable<? extends BasicDataModel> table) {
@@ -26,6 +29,7 @@ public class BasicTableSpeedSearch extends SpeedSearchBase<BasicTable<? extends 
 
     @NotNull
     @Override
+    @Compatibility
     protected Object[] getAllElements() {
         return getColumnInfos();
     }
@@ -45,26 +49,23 @@ public class BasicTableSpeedSearch extends SpeedSearchBase<BasicTable<? extends 
         return columnInfo.getName();
     }
 
-    public ColumnInfo[] getColumnInfos() {
-        if (columnInfos == null) {
-            DataModelHeader<? extends ColumnInfo> modelHeader = getTable().getModel().getHeader();
-            columnInfos = modelHeader.getColumnInfos().toArray(new ColumnInfo[modelHeader.getColumnCount()]);
-        }
-        return columnInfos;
+    private ColumnInfo[] createColumnInfos() {
+        DataModelHeader<? extends ColumnInfo> modelHeader = getTable().getModel().getHeader();
+        return modelHeader.getColumnInfos().toArray(new ColumnInfo[modelHeader.getColumnCount()]);
     }
 
     @Override
     protected void selectElement(Object o, String s) {
-        for(ColumnInfo columnInfo : columnInfos) {
-            if (columnInfo == o) {
-                columnIndex = columnInfo.getIndex();
-                BasicTable table = getTable();
-                int rowIndex = table.getSelectedRow();
-                if (rowIndex == -1) rowIndex = 0;
-                table.scrollRectToVisible(table.getCellRect(rowIndex, columnIndex, true));
-                table.setColumnSelectionInterval(columnIndex, columnIndex);
-                break;
-            }
+        for(ColumnInfo columnInfo : getColumnInfos()) {
+            if (columnInfo != o) continue;
+
+            columnIndex = columnInfo.getIndex();
+            BasicTable table = getTable();
+            int rowIndex = table.getSelectedRow();
+            if (rowIndex == -1) rowIndex = 0;
+            table.scrollRectToVisible(table.getCellRect(rowIndex, columnIndex, true));
+            table.setColumnSelectionInterval(columnIndex, columnIndex);
+            break;
         }
     }
 
