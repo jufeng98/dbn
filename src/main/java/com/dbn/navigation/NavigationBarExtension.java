8874433ef487d571fc41ab6cb0a00ae85df7d932
dancioca <dancioca@bluewin.ch>
2024-03-09 19:55:06 +0100

    DBNE-9320 Navigation bar - guarded logic

--- src/main/java/com/dbn/navigation/NavigationBarExtension.java
+++ src/main/java/com/dbn/navigation/NavigationBarExtension.java
@@ -13,9 +13,13 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.Processor;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.Collections;
+
+import static com.dbn.common.dispose.Checks.isValid;
+import static com.dbn.common.dispose.Failsafe.guarded;
 
 public class NavigationBarExtension extends AbstractNavBarModelExtension {
     @Override
@@ -29,22 +33,32 @@ public class NavigationBarExtension extends AbstractNavBarModelExtension {
 
     @Override
     public PsiElement getParent(PsiElement psiElement) {
-        if (psiElement instanceof DBObjectPsiFile || psiElement instanceof DBObjectPsiDirectory || psiElement instanceof DBConnectionPsiDirectory) {
-            return psiElement.getParent();
+        return guarded(null, psiElement, e -> parent(e));
+    }
+
+    private static @Nullable PsiElement parent(PsiElement psiElement) {
+        if (psiElement instanceof DBObjectPsiFile ||
+                psiElement instanceof DBObjectPsiDirectory ||
+                psiElement instanceof DBConnectionPsiDirectory) {
+
+            PsiElement parent = psiElement.getParent();
+            if (isValid(parent)) return parent;
         }
         return null;
     }
 
     @Override
     public PsiElement adjustElement(@NotNull PsiElement psiElement) {
+        return guarded(null, psiElement, e -> adjusted(e));
+    }
+
+    private static PsiElement adjusted(@NotNull PsiElement psiElement) {
         if (psiElement instanceof DBLanguagePsiFile) {
             DBLanguagePsiFile databaseFile = (DBLanguagePsiFile) psiElement;
             VirtualFile virtualFile = databaseFile.getVirtualFile();
             if (virtualFile instanceof DBVirtualFileBase) {
                 DBObject object = databaseFile.getUnderlyingObject();
-                if (object != null) {
-                    return DBObjectPsiCache.asPsiFile(object);
-                }
+                return DBObjectPsiCache.asPsiFile(object);
             }
         }
         return psiElement;
