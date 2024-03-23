d619d0a54c890d5f55c1f5850f6ee66270ff2450
Dan Cioca <dancioca@bluewin.ch>
2024-03-23 17:11:08 +0100

    Compatibility issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
   implementation("org.apache.poi:poi-ooxml-schemas:4.1.2")
 
   // poi library dependencies
-  implementation("org.apache.commons:commons-io:1.3.2")
+  implementation("org.apache.commons:commons-io:2.15.1")
   implementation("org.apache.commons:commons-math3:3.6.1")
   implementation("org.apache.commons:commons-compress:1.26.0")
   implementation("org.apache.commons:commons-collections4:4.4")
